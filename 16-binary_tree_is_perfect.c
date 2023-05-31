74e497a69054c5f9afb083dc18515d8c5b2766bc
JEPTUI <alicejeptui@gmail.com>
2023-05-31 04:04:14 -0700

    fix functions

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -61,38 +61,3 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 	return (is_perfect_recursive(tree, depth(get_leaf(tree)), 0));
 }
-/**
- * binary_tree_size - measures the size of a binary tree
- * @tree: pointer to the root node of the tree to measure the size
- * Return: If tree is NULL, the function must return 0 else size
- */
-size_t binary_tree_size(const binary_tree_t *tree)
-{
-	size_t size = 0;
-
-	if (!tree)
-		return (0);
-	if (tree)
-	{
-		size += 1;
-		size += binary_tree_size(tree->left);
-		size += binary_tree_size(tree->right);
-	}
-	return (size);
-}
-/**
- * binary_tree_height - measures the height of a binary tree
- * @tree: pointer to the root node of the tree to measure the height
- * Return: 0 if tree is NULL otherwise height
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	size_t left_h = 0, right_h = 0;
-
-	if (!tree)
-		return (0);
-	left_h = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	right_h = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-
-	return ((left_h > right_h) ? left_h : right_h);
-}
