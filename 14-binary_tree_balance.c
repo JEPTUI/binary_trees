e567221805b88113bc2ef6f5d374268483552c70
JEPTUI <alicejeptui@gmail.com>
2023-05-31 02:46:19 -0700

    Balance factor

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: pointer to the root node of the tree to measure the balance factor
+ * Return: If tree is NULL, return 0 else the balance factor
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left_h = 0, right_h = 0;
+
+	if (!tree)
+		return (0);
+
+	left_h = binary_tree_height(tree->left);
+	right_h = binary_tree_height(tree->right);
+
+	return (left_h - right_h);
+}
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
+ * Return: 0 if tree is NULL otherwise height
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_h = 0, right_h = 0;
+
+	if (!tree)
+		return (0);
+	left_h = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right_h = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+	return ((left_h > right_h) ? left_h : right_h);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -21,6 +21,9 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s heap_t;
 
 /** function prototypes **/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
@@ -38,5 +41,6 @@ size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
 
 #endif /* _BINARY_TREES_H_ */
