63135065c4bc1e1a8ffe6749e790e193ae10e296
JEPTUI <alicejeptui@gmail.com>
2023-05-31 20:01:36 -0700

    Fix betty style

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -11,7 +11,7 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 	binary_tree_t *first_par, *sec_par;
 
 	if (!first || !second)
-		return(NULL);
+		return (NULL);
 	if (first == second)
 		return ((binary_tree_t *)first);
 
